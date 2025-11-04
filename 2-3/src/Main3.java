bb92b973c6e5f810aaface062834b6ce069a0a99
榊原　寛隆 <hiro20030409@gmail.com>
2025-11-04 10:32:50 +0900

    Add files via upload

---
+++ 2-3/src/Main3.java
@@ -0,0 +1,9 @@
+/*このプログラムはHelloWorldと表示させるプログラムです */
+public class Main3 {
+    /**Main3のクラスです */
+    public static void main(String[] args) {
+
+ //この下のコードがHelloWerldと表示させるコードです
+        System.out.println("Hello, World!");
+    } 
+}
