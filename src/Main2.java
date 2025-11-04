03f16389f2190f1bd43f39eb4081b12abfad94a2
榊原　寛隆 <hiro20030409@gmail.com>
2025-11-04 10:36:27 +0900

    Add files via upload
    2-3の課題提出しますので確認よろしくお願い致します。

---
+++ src/Main2.java
@@ -0,0 +1,8 @@
+public class Main2 {
+    public static void main(String[] args) {
+        byte test = 85;
+        int attendancenumber = 23;
+        boolean passed = true;
+        String name = "山田太郎";
+    }
+}
---
+++ src/Main4.java
@@ -0,0 +1,7 @@
+public class Main4 {
+    public static void main(String[] args) {
+        System.out.println("彼は\"javaprograming\"が好きです \n\t次の行です \n\tタブを使用しています。");
+
+    }
+
+}
