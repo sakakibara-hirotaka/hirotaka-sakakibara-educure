68e49b83fab7cf514b8d80a0eb5ddaf4bfa8e3df
榊原　寛隆 <hiro20030409@gmail.com>
2025-11-06 09:53:51 +0900

    Delete 2-3 directory

--- 2-3/src/Main3.java
+++
@@ -1,9 +0,0 @@
-/*このプログラムはHelloWorldと表示させるプログラムです */
-public class Main3 {
-    /**Main3のクラスです */
-    public static void main(String[] args) {
-
- //この下のコードがHelloWerldと表示させるコードです
-        System.out.println("Hello, World!");
-    } 
-}
