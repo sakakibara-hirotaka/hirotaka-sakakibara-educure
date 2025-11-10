b021bb89e91d659fade121de083cf9fb64b8c49f
榊原　寛隆 <hiro20030409@gmail.com>
2025-11-10 16:53:23 +0900

    Add files via upload

---
+++ 2-12/src/Main3.java
@@ -0,0 +1,20 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int height = 7;
+
+        //段
+        for (int r = 1; r <= height; r++) {
+            //空白
+            for (int space = 1; space <= height - r; space++) {
+                System.out.print(" ");
+            }
+            //星
+            for (int i = 1; i <= 2 * r - 1; i++)  {
+                System.out.print("*");  
+            }
+
+            System.out.println();
+        }
+
+    }
+}
