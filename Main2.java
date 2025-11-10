e933c7348c52f767733156ed9f0f0b7114fd34a3
榊原　寛隆 <hiro20030409@gmail.com>
2025-11-10 10:37:44 +0900

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=5; i++) {
+            for (int t =1; t <= 9; t++) {
+                System.out.println(i + " × " + t +" = " + i*t);
+            }
+            System.out.println();
+        }  
+
+    }
+}
