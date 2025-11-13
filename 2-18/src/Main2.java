b950145936aa7aac18027b5b3120db3135eb021b
榊原　寛隆 <hiro20030409@gmail.com>
2025-11-13 13:58:03 +0900

    Add files via upload

---
+++ 2-18/src/Main2.java
@@ -0,0 +1,22 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int score = 85;  // 学生の得点
+
+        //結果受け取り
+        String result = getGrade(score);
+        System.out.println("成績: " + result);
+    }
+
+    public static String getGrade(int score) {
+        if (score >= 80) {
+            return "優";
+        } else if (score >= 70) {
+            return "良";
+        } else if (score >= 60) {
+            return "可";
+        } else {
+            return "不可";
+        }    
+
+    }
+}    
