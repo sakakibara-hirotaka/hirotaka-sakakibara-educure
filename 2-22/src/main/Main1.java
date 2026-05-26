f58537308dd9bc29a11b9cf15ef8fa0e69b0da34
榊原　寛隆 <hiro20030409@gmail.com>
2026-05-26 09:38:16 +0900

    Add files via upload

---
+++ 2-22/src/main/Main1.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.util.Scanner;
+import wordregister.WordRegister;
+import quiz.Quiz;
+
+public class  Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[][] words = null;
+
+        //メニュー画面
+        while (true) {
+            System.out.println("1: 単語を登録する");
+            System.out.println("2: クイズを受ける");
+            System.out.println("3: 終了する");
+
+            if (!scanner.hasNextInt()) {
+                System.out.println("1-3の数字を入力してください。\n");
+                scanner.next();
+                continue;
+            }
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1) {
+                words = WordRegister.tester();
+            } else if (choice == 2) {
+                if (words == null) {
+                    System.out.println("単語が登録されていません。\n");
+                } else {
+                    Quiz.quiz(words);
+                    }
+            } else if (choice == 3) {
+                break;
+            } else {
+                System.out.println("1-3の数字を入力してください。\n");
+            }
+        }
+        scanner.close();
+    }
+}
---
+++ 2-22/src/wordregister/WordRegister.java
@@ -0,0 +1,47 @@
+package wordregister;
+
+import java.util.Scanner;
+
+public class WordRegister {
+
+    static int counter = 0;
+    static String[] englishWords = new String[20];
+    static String[] japaneseWords = new String[20];
+
+    public static String[][] tester() {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            boolean duplication = true;
+            if (counter >= 20) {
+                System.out.println("登録可能な単語数は20個までです。");
+                break;
+            }
+
+            System.out.print("英単語を入力してください:\n");
+            String eng = scanner.nextLine();
+                
+            //重複チェック
+            for (int i =0; i<counter; i++) {
+                if (englishWords[i].equals(eng)) {
+                System.out.println("すでに入力されています");
+                duplication = false;
+                }
+            }
+
+            if (duplication == false) {
+                continue;
+            }
+
+            System.out.print("日本語訳を入力してください:\n");
+            String jp = scanner.nextLine();
+
+            // 配列に保存
+            englishWords[counter] = eng;
+            japaneseWords[counter] = jp;
+            counter++;
+            
+        }
+        return new String[][] {englishWords, japaneseWords };
+    }
+}
