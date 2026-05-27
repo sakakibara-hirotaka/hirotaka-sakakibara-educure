12631381c50cf281faf1520dfcffd5d1f1bc1406
榊原　寛隆 <hiro20030409@gmail.com>
2026-05-27 12:11:16 +0900

    Add files via upload

---
+++ 3-8/src/Main1/Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] artgs) {
+
+        Sword sword = new Sword("ブロンズソード",10);
+
+        System.out.println("武器名: " + sword.getName());
+        System.out.println("攻撃力: " + sword.getPower());
+    }
+}
