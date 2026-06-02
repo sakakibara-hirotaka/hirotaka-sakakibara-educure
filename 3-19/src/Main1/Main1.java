a52058c408729e8f83aec9370de6bb428a2eeca8
榊原　寛隆 <hiro20030409@gmail.com>
2026-06-02 14:09:08 +0900

    Add files via upload

---
+++ 3-19/src/Main1/Main1.java
@@ -0,0 +1,10 @@
+package Main1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        
+    Product product = new Product("コーヒー" , 380);
+
+    System.out.println(product);
+    }
+}
