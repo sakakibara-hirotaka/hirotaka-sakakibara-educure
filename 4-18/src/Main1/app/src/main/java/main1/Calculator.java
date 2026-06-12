0a73bf503786e0da06a7ff11f5766f82ea6f8d58
榊原　寛隆 <hiro20030409@gmail.com>
2026-06-12 15:15:00 +0900

    Add files via upload

---
+++ 4-18/src/Main1/app/src/main/java/main1/Calculator.java
@@ -0,0 +1,12 @@
+package main1;
+
+public class Calculator {
+
+    public int add(int a , int b) {
+        return a+b;
+    }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+}
