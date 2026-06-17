11b80b920e7cb4dc40cc4d544c30109b5fd8a220
榊原　寛隆 <hiro20030409@gmail.com>
2026-06-17 11:34:14 +0900

    Add files via upload

---
+++ 4-22/src/Main4/Employee.java
@@ -0,0 +1,18 @@
+package Main4;
+
+public class Employee {
+    
+    private String name;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age; 
+    }
+    public double getSalary() {
+        return salary;
+    }
+}
