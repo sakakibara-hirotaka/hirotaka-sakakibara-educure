5fd72a965459e656cce4afab2f43eb236be70f1b
榊原　寛隆 <hiro20030409@gmail.com>
2026-06-19 10:03:56 +0900

    Add files via upload

---
+++ 4-27/src/Main2/Main2.java
@@ -0,0 +1,40 @@
+package Main2;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Main2 {
+    public static void main(String[] args) {
+        
+        try {
+            String query = "title = java";
+            String encodedQuery = URLEncoder.encode(query, "UTF-8");
+            String endpoint = "https://ndlsearch.ndl.go.jp/api/sru";
+            String requestUrl = endpoint + "?operation=searchRetrieve" + "&version=1.2" + "&query=" + encodedQuery + "&maximumRecords=10" + "&recordSchema=dc";
+
+            URL url = new URL(requestUrl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            InputStream is= conn.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is); 
+            BufferedReader br = new BufferedReader(isr);
+            String read = br.readLine();
+
+            while(read != null) {
+                System.out.println(read);
+                read = br.readLine();
+            }
+            isr.close();    
+
+        
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+    }
+}
