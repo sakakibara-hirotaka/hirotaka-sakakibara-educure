19deefbdefabd9b03718ec521b845e8739d27caa
榊原　寛隆 <hiro20030409@gmail.com>
2026-06-24 11:11:31 +0900

    Add files via upload

---
+++ 4-28/src/Main1/FileHandler.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class FileHandler  {
+    
+    public void exportToCSV(List<Word> words, String filename) throws IOException{
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))){
+            for (Word word : words) {
+                bw.write(word.getEnglish() + "," + word.getJapanese() +"\n");
+            }
+        } 
+    }
+
+    public void importFromCSV(String filename, WordManager wordManager) throws IOException{
+
+        try (FileReader reader = new FileReader(filename);
+            BufferedReader br = new BufferedReader(reader)){
+                
+            String line;
+
+            while((line = br.readLine()) != null) {
+            String[] values = line.split(",");
+
+            if (values.length < 2) {
+                throw new IOException("CSVの形式が不正です");
+            }
+            
+            Word word = new Word(values[0], values[1]);
+            wordManager.addWord(word);
+            }   
+        } 
+    }
+}
