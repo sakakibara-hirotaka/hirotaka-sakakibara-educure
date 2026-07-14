a64fe53ff18df52a9d894520a23a7964bc11c91a
榊原　寛隆 <hiro20030409@gmail.com>
2026-07-14 11:19:01 +0900

    Update PaypalProcessor.java

--- 7-6/Question4/paymentprocessor/src/main/java/jp/educure/probrem4/paymentprocessor/payment/PaypalProcessor.java
+++ 7-6/Question4/paymentprocessor/src/main/java/jp/educure/probrem4/paymentprocessor/payment/PaypalProcessor.java
@@ -7,6 +7,6 @@ public class PaypalProcessor implements PaymentProcessor{
     
     @Override
     public void processPayment(double amount) {
-        System.out.println("PayPalで支払いを処理中:" + amount);
+        System.out.println("PayPalで支払いを処理中: ¥" + amount);
     }
 }
