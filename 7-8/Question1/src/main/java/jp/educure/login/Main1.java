d462c9ab62379e2371b922ac570c0700ff7654e3
榊原　寛隆 <hiro20030409@gmail.com>
2026-07-28 13:15:32 +0900

    Add files via upload

---
+++ 7-8/Question1/src/main/java/jp/educure/login/Main1.java
@@ -0,0 +1,35 @@
+package jp.educure.login;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main1 {
+
+	private final UserSession userSession;
+
+    Main1(UserSession userSession) {
+        this.userSession = userSession;
+    }
+
+    public static void main(String[] args) {
+		SpringApplication.run(Main1.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(UserService userService) {
+		return args -> {
+			try {
+				userSession.login();
+				//userSession.logout();
+				userService.getUserDetails();
+			} catch (IllegalStateException e) {
+				// TODO: handle exception
+				System.out.println("Exception: " + e.getMessage());
+			}
+			
+		};
+	}
+}
