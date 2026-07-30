c831c8d70470630d27ff0da7dd28e1d15dc48d4a
榊原　寛隆 <hiro20030409@gmail.com>
2026-07-30 09:06:39 +0900

    Add files via upload

---
+++ 7-8/Question3/management/src/main/java/jp/educure/management/User.java
@@ -0,0 +1,29 @@
+package jp.educure.management;
+
+public class User {
+
+    private String name;
+    private String mail;
+
+
+    public User(String name, String mail) {
+        this.name = name;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+    
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
