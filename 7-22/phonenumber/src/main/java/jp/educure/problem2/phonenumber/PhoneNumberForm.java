c534deab7b9f6d297eb5cf4f4b9e1a6de3102b87
榊原　寛隆 <hiro20030409@gmail.com>
2026-08-20 16:06:14 +0900

    Add files via upload

---
+++ 7-22/phonenumber/src/main/java/jp/educure/problem2/phonenumber/PhoneNumberForm.java
@@ -0,0 +1,14 @@
+package jp.educure.problem2.phonenumber;
+
+public class PhoneNumberForm {
+
+    private String number;
+    
+    public String getNumber() {
+        return number;
+    }
+    
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
---
+++ 7-22/productstock/src/main/java/jp/educure/problem1/productstock/ProductStockForm.java
@@ -0,0 +1,14 @@
+package jp.educure.problem1.productstock;
+
+public class ProductStockForm {
+
+    private Integer stock;
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
